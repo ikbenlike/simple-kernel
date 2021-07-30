fe5ce3786b828e858f82e915fa49bce5860be846
ikbenlike <thewatcheriswatchinu@gmail.com>
2021-07-30 21:00:09 +0200

    add optional extern C to tty.h

--- kernel/include/kernel/tty.h
+++ kernel/include/kernel/tty.h
@@ -2,9 +2,18 @@
     #define _KERNEL_TTY_H_
     
     #include <stddef.h>
+
+    #ifdef __cplusplus
+        extern "C" {
+    #endif
+
     void terminal_initialize(void);
     void terminal_putchar(char c);
     void terminal_write(const char *data, size_t size);
     void terminal_writestring(const char *data);
+
+    #ifdef __cplusplus
+        }
+    #endif
 
 #endif
