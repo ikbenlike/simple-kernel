bee58def2a975f3a723cf4f15a148731d8997ec1
ikbenlike <thewatcheriswatchinu@gmail.com>
2021-08-01 00:01:39 +0200

    pass multiboot values to kernel_main; move VGA buffer mapping to kernel_main

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <stdio.h>
 
+#include <kernel/multiboot.h>
 #include <kernel/tty.h>
 #include <kernel/mem.h>
 
@@ -14,10 +15,24 @@
 #error "This tutorial needs to be compiled with a ix86-elf compiler"
 #endif
 
-void kernel_main(void){
+void kernel_main(uint32_t multiboot_magic, void *multiboot_header){
+    //Map VGA buffer in virtual memory. If mapping fails, silently exit.
+    if(map_page((void*)0x000B8000, (void*)0xC03FE000, 0x103)){
+        return;
+    }
+
     terminal_initialize();
 
-    void *pa = get_physaddr(0xC03FE000);
+    if(multiboot_magic != MULTIBOOT_MAGIC){
+        terminal_writestring("This kernel requires a multiboot-compliant bootloader.");
+    }
+
+    // Physical address. Must first be mapped to virtual page to be useful for us.
+    struct multiboot_info *mbinfo = (struct multiboot_info*)multiboot_header;
+    init_frame_allocator(mbinfo);
+
+
+    void *pa = get_physaddr((void*)0xC03FE000);
 
     if(pa == (void*)(0x000B8000)){
         terminal_writestring("aye");
