b0807524c68a335deed400cecce72516e5170f68
ikbenlike <thewatcheriswatchinu@gmail.com>
2021-08-14 23:06:35 +0200

    add check to see if fault originates in userspace or not

--- kernel/arch/i386/interrupts.c
+++ kernel/arch/i386/interrupts.c
@@ -3,15 +3,22 @@
 #include <kernel/tty.h>
 #include <kernel/idt.h>
 
+extern void iprint(uint64_t);
 
-void interrupt_handler(struct stackframe sf){
-    terminal_writestring("interrupt fired!\n");
-    if(sf.err == 0){
-        terminal_writestring("error seems to be working too\n");
+void zero_division_handler(struct stackframe sf){
+    if((sf.cs & 0b11) == 3){
+        //TODO when userspace gets added: terminate offending program
+    }
+    else {
+        terminal_writestring("FATAL ERROR: division-by-zero occurred in kernel-space.");
+        while(1){};
     }
 }
 
 void reserved_interrupt(struct stackframe fs){
+    if((sf.cs & 0b11) == 3){
+        //TODO terminate program
+    }
     terminal_writestring("FATAL ERROR: this isn't supposed to happen! Reserved interrupt fired.");
 
     while(1){};
