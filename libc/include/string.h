1e35c3288a30ecb24249382bb2390505314186c6
ikbenlike <thewatcheriswatchinu@gmail.com>
2021-08-22 03:51:30 +0200

    att strcpy() to libc implementation

--- libc/include/string.h
+++ libc/include/string.h
@@ -12,6 +12,8 @@
     void *memcpy(void *restrict dstptr, const void *restrict srcptr, size_t size);
     int memcmp(const void *aptr, const void *bptr, size_t size);
 
+    char *strcpy(char *restrict dest, const char *src);
+
     #ifdef __cplusplus
         }
     #endif
---
+++ libc/string/strcpy.c
@@ -0,0 +1,11 @@
+#include <string.h>
+
+char *strcpy(char *restrict dest, const char *src){
+    int i = 0;
+    while(src[i] != '\0'){
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return dest;
+}
