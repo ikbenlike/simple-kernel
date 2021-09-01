5a66233d0f1c80336515cf02fce70ed4e6acab05
ikbenlike <thewatcheriswatchinu@gmail.com>
2021-09-01 19:50:12 +0200

    add strcmp() implementation to libk

--- libc/include/string.h
+++ libc/include/string.h
@@ -6,13 +6,14 @@
         extern "C" {
     #endif
 
-    size_t strlen(const char *str);
-    void *memset(void *bufptr, int value, size_t size);
-    void *memmove(void* dstptr, const void* srcptr, size_t size);
-    void *memcpy(void *restrict dstptr, const void *restrict srcptr, size_t size);
     int memcmp(const void *aptr, const void *bptr, size_t size);
+    void *memcpy(void *restrict dstptr, const void *restrict srcptr, size_t size);
+    void *memmove(void* dstptr, const void* srcptr, size_t size);
+    void *memset(void *bufptr, int value, size_t size);
 
+    int strcmp(const char *s1, const char *s2);
     char *strcpy(char *restrict dest, const char *src);
+    size_t strlen(const char *str);
 
     #ifdef __cplusplus
         }
